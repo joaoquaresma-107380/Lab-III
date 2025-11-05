c0d4835388519b822874084408870a25a2b9fae2
mariana106889 <a106889@uminho.pt>
2025-11-05 21:07:34 +0000

    Update Manager_Aeroportos.h

--- Manager_Aeroportos.h
+++ Manager_Aeroportos.h
@@ -10,9 +10,9 @@ typedef struct manager_Aeroportos Manager_Aeroportos;
 
 void value_destroy_func(gpoint p);
 Manager_Aeroportos* createManagerAeroportos();
-gboolean adicionaAeroporto(Aeroporto* aeroporto, Manager_Aeroportos* manager);
+void adicionaAeroporto(Aeroporto* aeroporto, Manager_Aeroportos* manager);
 Aeroporto* procurarReserva(Manager_Aeroportos* gestor, char* code);
 GList * todasReservas (Manager_Aeroportos* gestor);
-gboolean removeAeroporto(Aeroporto* aeroporto, Manager_Aeroportos* manager);
+void removeAeroporto(Aeroporto* aeroporto, Manager_Aeroportos* manager);
 
 #endif
