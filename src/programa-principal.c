3beba009dfba5ed886c518547914bfc70680516c
joaoquaresma-107380 <a107380@uminho.pt>
2025-11-11 19:12:15 +0000

    Add files via upload

--- src/programa-principal.c
+++ src/programa-principal.c
@@ -1,26 +1,47 @@
-#include "string.h"
-#include "stdio.h"
-#include <Manager_Aeroportos.h>
-#include <Manager_Reservas.h>
-#include <Manager_Voos.h>
-#include <gestorAeronave.h>
-#include <gestorPassageiro.h>
-#include <aeroportoParser.h>
-#include <aeronaveParser.h>
-#include <reservaParser.h>
-#include <vooParser.h>
-#include <passageirosParser.h>
+#include <string.h>
+#include "AeroportoParser.h"
+#include "AeronaveParser.h"
+#include "ReservaParser.h"
+#include "VooParser.h"
+#include "PassageirosParser.h"
 
-int main(int argc,char args[]) {
-    if(argc < 3){
-        perror("Número de argumentos inválido!");
-    }
 
-    Manager_Reservas* reversas = createManagerReservas();
+void programa_principal(const char* dataSet, const char* comandos_exe) {
+    FILE* airports = abrirFicheiroLeitura(strcat(dataSet, "/airports.csv"));
+    FILE* aircrafts = abrirFicheiroLeitura(strcat(dataSet, "/aircrafts.csv"));
+    FILE* passengers = abrirFicheiroLeitura(strcat(dataSet, "/passengers.csv"));
+    FILE* flights = abrirFicheiroLeitura(strcat(dataSet, "/flights.csv"));
+    FILE* reservations = abrirFicheiroLeitura(strcat(dataSet, "/reservations.csv"));
     Manager_Aeroportos* aeroportos = createManagerAeroportos();
-    Manager_Voos* voos = createManager_Voos();
-    gestorAviao* avioes = criarGestorAviao();
-    gestorPassageiros* passageiros = createGestorPassageiros();
-    
+    readFileAeroporto(airports, aeroportos);
+    fecharFicheiro(airports);
+    Manager_Aeronaves* aeronaves = createManagerAeronaves();
+    readFileAeronave(aircrafts, aeronaves);
+    fecharFicheiro(aircrafts);
+    Manager_Passageiros* passageiros = createManagerPassageiros();
+    readFilePassageiro(passengers, passageiros);
+    fecharFicheiro(passengers);
+    Manager_Voos* voos = createManagerVoos();
+    readFileVoo(flights, voos, aeronaves);
+    fecharFicheiro(flights);
+    Manager_Reservas* reservas = createManagerReservas();
+    readFileReserva(reservations, reservas, voos, passageiros);
+    fecharFicheiro(reservations);
+
+    // ja esta tudo na base de dados e corrigiddo
+    // agora é passar os comandos para as queries
+}
+
+
+
+
+int main(char args[],int argc) {
+    if (args[0] == "progrma_principal") {
+        programa_principal(args[1],args[2]);
+    }
+    if(args[0] == "programa_iterativo") {
+
+    }
+    if()
     return 0;
 }
