e9e784337537342b8533c6da281b9c4a9fb0ce77
mariana106889 <a106889@uminho.pt>
2025-11-11 19:22:19 +0000

    Add files via upload

--- include/comandoParser.h
+++ include/comandoParser.h
@@ -5,15 +5,16 @@
 #include "Manager_Aeroportos.h"
 #include "Manager_Aeronaves.h"
 #include "Manager_Voos.h"
+#include "Data.h"
 
 typedef struct cmd {
     int comando;
-    int* args; // NULL se nao houver
+    char** args; // NULL se nao houver
     int nargs; // numero de argumentos
 } Cmd;
 
-void processaCommando(Deque* deque, Cmd* cmd);
-
+void processaCommando(Manager_Aeroportos* aeroportos, GestorAviao* aeronaves, Manager_Voos* voos, Cmd* cmd, File* resultados);
 Cmd* parseLineCmd(char* line);
+void readFileComandos(FILE* stream, Manager_Aeroportos* ma, Manager_Voos* mv, GestorAviao* mav);
 
 #endif
